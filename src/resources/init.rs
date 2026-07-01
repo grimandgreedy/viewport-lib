5239b284608b6ef61c7549e0db3085fcf12e1a84
Grim <grimandgreedy@protonmail.com>
2026-07-02 01:52:26 +1000

    Revert "Generate mipmaps on material texture uploads"
    This reverts commit 6f75835fe0d6d9b191edb86a74b611a9dfa49fb4.

--- src/resources/init.rs
+++ src/resources/init.rs
@@ -1730,9 +1730,7 @@ impl ViewportGpuResources {
             address_mode_v: wgpu::AddressMode::Repeat,
             mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
-            // Trilinear: material textures are uploaded with a full mip chain,
-            // so blend between mip levels instead of snapping to the nearest.
-            mipmap_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
 
